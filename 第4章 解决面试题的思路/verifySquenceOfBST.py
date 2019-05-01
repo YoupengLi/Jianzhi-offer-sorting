7405467c3b7d2d1c698977f698c7f8831fd1f3b4
YoupengLi <31504038+YoupengLi@users.noreply.github.com>
2019-05-01 15:05:28 +0800

    Add files via upload

--- 第4章 解决面试题的思路/verifySquenceOfBST.py
+++ 第4章 解决面试题的思路/verifySquenceOfBST.py
@@ -8,10 +8,8 @@
 '''
 题目：输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。如果是则返回True,否则返回False。
 假设输入的数组的任意两个数字都互不相同。
-
 1. 二叉搜索树即是二叉排序树，二叉搜索树左子树上所有的结点均小于根结点、右子树所有的结点均大于根结点；
 2. 后序遍历序列的最后一个元素为二叉树的根节点。
-
 算法步骤如下：
 1. 找到根结点；
 2. 遍历序列，找到第一个大于等于根结点的元素i，则i左侧为左子树、i右侧为右子树；
@@ -28,21 +26,21 @@ class Solution:
         for node in seq[:-1]:
             if node > root:
                 break
-            i += 1
+            i += 1              # i为右子树第一个值的索引
         for node in seq[i:-1]:
             if node < root:
                 return False
 
         # 判断左子树是否为二叉搜索树
         left = True
-        # i>0 意味i=0或者1的时候，两个元素在二叉树没有排序之分的，但是3个元素就有了左右子树之分
+        # i>0 意味左树至少有一个元素:seq[0]
         if i > 0:
             left = self.verifySquenceOfBST(seq[:i])
 
         right = True
-        # len(sequence)>3才有左右之分的
-        if i < len(seq) - 2 and left:
-            right = self.verifySquenceOfBST(seq[i + 1:])
+        # i<len(seq)-2 意味右树至少有一个元素：seq[len(seq)-2]
+        if left and i < len(seq) - 2:
+            right = self.verifySquenceOfBST(seq[i+1:-1])
 
         return left and right
 
